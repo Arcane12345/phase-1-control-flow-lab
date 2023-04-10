c4e1ad47d83dcf24498416c064d872ee1d2dd263
Eric <macario41@gmail.com>
2023-04-10 14:08:59 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400){
+    return "This one is on me!"
+  }
+ 
+  else if (400 < someValue && someValue< 2000) {
+    return "That will be twenty bucks."
+  }
+
+  else if (2000 <= someValue && someValue < 2500) {
+    return "I will gladly take your thirty bucks."
+  }
+  
+  else (someValue > 2500) ;{
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === "NYC"){
+    return "Ok, sounds good."
+  }
+  else return "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(generousness){
+  if(generousness === "generous"){
+    return "Thank you so much."
+  }
+  else if (generousness === "not as generous"){
+    return "Thank you."
+  }
+  else return "Bye."
 }
